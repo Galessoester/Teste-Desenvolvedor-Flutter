af30cc26c763f10254b9544ddaa5a082b2e0f7d3
Ester Galesso <galessoester@gmail.com>
2023-12-07 20:38:07 -0300

    configuração supabase

---
+++ lib/services/supabase.dart
@@ -0,0 +1,6 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final supabase = SupabaseClient(
+  'https://nutiskzefogznzkndlyf.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51dGlza3plZm9nem56a25kbHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE5MTkxMTIsImV4cCI6MjAxNzQ5NTExMn0.ZeXlC0dq5csihsXMe_06CzuOmKWu73gi_GRZIQRzMpk',
+);
